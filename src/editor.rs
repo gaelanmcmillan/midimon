c29fd647d83bf4595791556fe052bfbadb69c55d
Gaelan McMillan <gaelan.mcmillan@gmail.com>
2023-02-15 10:10:37 -0500

    add a scrollview

--- src/editor.rs
+++ src/editor.rs
@@ -14,6 +14,9 @@ use std::time::{Duration, SystemTime};
 
 // In the editor, we want our MIDI list to update on possibly every process block, reflecting the MIDI we've monitored so far.
 
+mod message_list;
+mod piano_roll;
+
 #[derive(Lens, Clone)]
 pub(crate) struct EditorData {
     pub(crate) params: Arc<MidimonParams>,
@@ -24,11 +27,11 @@ pub(crate) struct EditorData {
 impl Model for EditorData {}
 
 pub(crate) fn default_state() -> Arc<ViziaState> {
-    ViziaState::from_size(400, 200)
+    ViziaState::from_size(400, 400)
 }
 
 pub(crate) fn create(
-    editor_data: EditorData,
+    mut editor_data: EditorData,
     editor_state: Arc<ViziaState>,
 ) -> Option<Box<dyn Editor>> {
     create_vizia_editor(editor_state, ViziaTheming::Custom, move |cx, _| {
@@ -36,7 +39,12 @@ pub(crate) fn create(
         assets::register_noto_sans_thin(cx);
 
         let mut buf = editor_data.midi_consumer.lock().unwrap();
+        // let num_free = buf.slots();
+        // let buf = buf.read_chunk(crate::MIDI_BUFFER_SIZE - num_free);
+
         let mut notes: Vec<TimeNotePair> = vec![];
+
+        // for time_note_pair in buf.iter() {}
 
         while let Ok(time_note_pair) = buf.pop() {
             log!(Level::Info, "Popped note from UI thread");
@@ -45,7 +53,12 @@ pub(crate) fn create(
 
         EditorData {
             params: editor_data.params.clone(),
-            midi_history: notes,
+            midi_history: editor_data
+                .midi_history
+                .iter()
+                .cloned()
+                .chain(notes.into_iter())
+                .collect(),
             midi_consumer: editor_data.midi_consumer.clone(),
         }
         .build(cx);
@@ -61,41 +74,46 @@ pub(crate) fn create(
                 .font_size(30.0)
                 .height(Pixels(50.0));
 
-            List::new(cx, EditorData::midi_history, move |cx, _index, entry| {
-                log!(Level::Info, "Drawing a list item");
-                let (time, message) = entry.get(cx);
-                let formatted_time = DateTime::<Local>::from(time).format("%r");
+            ScrollView::new(cx, 0., 0., false, true, |cx| {
+                log!(Level::Info, "Drawing a ScrollView");
+                List::new(cx, EditorData::midi_history, move |cx, _index, entry| {
+                    log!(Level::Info, "Drawing a list item");
+                    let (time, message) = entry.get(cx);
+                    let formatted_time = DateTime::<Local>::from(time).format("%r");
 
-                match message {
-                    NoteEvent::NoteOn {
-                        timing,
-                        voice_id: _,
-                        channel,
-                        note,
-                        velocity,
-                    } => Label::new(
-                        cx,
-                        &format!(
-                            "[{}] Note On: {} {} {} {}",
-                            formatted_time, timing, channel, note, velocity
+                    // Draw different labels for different messages
+                    match message {
+                        NoteEvent::NoteOn {
+                            timing,
+                            voice_id: _,
+                            channel,
+                            note,
+                            velocity,
+                        } => Label::new(
+                            cx,
+                            &format!(
+                                "[{}] Note On: {} {} {} {}",
+                                formatted_time, timing, channel, note, velocity
+                            ),
                         ),
-                    ),
-                    NoteEvent::NoteOff {
-                        timing,
-                        voice_id: _,
-                        channel,
-                        note,
-                        velocity,
-                    } => Label::new(
-                        cx,
-                        &format!(
-                            "[{}] Note Off: {} {} {} {}",
-                            formatted_time, timing, channel, note, velocity
+                        NoteEvent::NoteOff {
+                            timing,
+                            voice_id: _,
+                            channel,
+                            note,
+                            velocity,
+                        } => Label::new(
+                            cx,
+                            &format!(
+                                "[{}] Note Off: {} {} {} {}",
+                                formatted_time, timing, channel, note, velocity
+                            ),
                         ),
-                    ),
-                    _ => Label::new(cx, "Unrecognized Message Type"),
-                };
-            });
+                        _ => Label::new(cx, "Unrecognized Message Type"),
+                    };
+                });
+            })
+            .size(Pixels(380.));
         });
     })
 }
